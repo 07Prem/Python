a1129018c0a5b092119389717e84c87aa655ea5e
07Prem <your_github_email@example.com>
2025-11-20 21:33:22 +0530

    first commit

---
+++ Exp-17/17Q2post.py
@@ -0,0 +1,10 @@
+import sympy as sp
+
+n, z = sp.symbols('n z')
+
+x_n = 2**n
+
+X_z = sp.summation(x_n * z**(-n), (n, 0, sp.oo))
+
+print("Z-transform of x[n] = 2^n u[n]:")
+sp.pprint(X_z, use_unicode=True)
